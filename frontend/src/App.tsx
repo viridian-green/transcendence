131255e6d92b215e5ccd2cd5cd02974e6ec3e0f8
Petra Bencze <petra.bncz@gmail.com>
2025-12-07 11:03:50 +0100

    test: husky

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
 import './App.css'
 
+const unusedVariable = 42;
+
 function App() {
+
+      const unusedString = "Does prettier work?";
   return (
     <div className='text-2xl font-bold text-center'>Welcome to Transcendence! 🏓</div>
   )
