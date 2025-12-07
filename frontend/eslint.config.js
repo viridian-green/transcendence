654a4f8728cb0348aed2e9a7e6ca7ca0a4e528a1
Petra Bencze <petra.bncz@gmail.com>
2025-12-07 11:31:46 +0100

    fix: pre-commit hook

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,30 +1,32 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import prettier from 'eslint-plugin-prettier';
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx,js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      prettier,
-    },
-    rules: {
-      'prettier/prettier': 'error',
-      'semi': ['warn', 'always'],
-    },
-  },
-])
+	globalIgnores(['dist', 'node_modules', 'build']),
+	{
+		files: ['**/*.{ts,tsx,js,jsx}'],
+		extends: [
+			js.configs.recommended,
+			tseslint.configs.recommended,
+			reactHooks.configs.flat.recommended,
+			reactRefresh.configs.vite,
+		],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.browser,
+			sourceType: 'module',
+		},
+		plugins: {
+			prettier,
+		},
+		rules: {
+			'prettier/prettier': 'error',
+			semi: ['warn', 'always'],
+		},
+	},
+]);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,6 @@
 import './App.css';
 
 function App() {
-	const variable = 'This should be formatted by prettier.';
-	console.log(variable);
 	return <div className='text-2xl font-bold text-center'>Welcome to Transcendence! 🏓</div>;
 }
 
