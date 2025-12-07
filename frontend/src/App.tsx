3b55e887d9ccef8bc01d7da5a81ab91ed0a0486a
Petra Bencze <petra.bncz@gmail.com>
2025-12-07 11:29:55 +0100

    test: husky

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,9 @@
-import './App.css'
-
-const unusedVariable = 42;
+import './App.css';
 
 function App() {
-
-      const unusedString = "Does prettier work?";
-  return (
-    <div className='text-2xl font-bold text-center'>Welcome to Transcendence! 🏓</div>
-  )
+	const variable = 'This should be formatted by prettier.';
+	console.log(variable);
+	return <div className='text-2xl font-bold text-center'>Welcome to Transcendence! 🏓</div>;
 }
 
-export default App
+export default App;
