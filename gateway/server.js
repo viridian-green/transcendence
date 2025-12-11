3242810d632e94e432c3abf7dd088b8377425d5c
Daiana <dairotchen@gmail.com>
2025-12-11 14:23:01 +0100

    feat: create gateway

---
+++ gateway/server.js
@@ -0,0 +1,26 @@
+//const { registerUserModule } = require("./services/user");
+const { registerGameModule } = require("./services/game");
+
+const fastify = require("fastify")({ logger: true });
+PORT = 3000;
+HOST = "0.0.0.0";
+
+// Register routes
+//fastify.register(require("./routes/users"));
+//fastify.register(require(".services/game"));
+//fastify.register(require('.services/tournament'))
+
+const start = async () => {
+  try {
+    // Register modules
+    //await registerUserModule(fastify);
+    await registerGameModule(fastify);
+
+    await fastify.listen({ port: PORT, host: HOST });
+  } catch (error) {
+    fastify.log.error(error);
+    process.exit(1);
+  }
+};
+
+start();
