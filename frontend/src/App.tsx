ea7187f456113e8b352566b212380b23ce61dc1f
Daiana <dairotchen@gmail.com>
2025-12-11 14:23:39 +0100

    feat: add iframe game to index page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
 import './App.css';
 
 function App() {
-	return <div className='text-center text-2xl font-bold'>Welcome to Transcendence! 🏓</div>;
+	return (
+		<div className='text-center text-2xl font-bold mt-8'>
+			Welcome to Transcendence! 🏓
+			<div className='mt-8 flex justify-center'>
+				<iframe
+					src='http://localhost:3000/game'
+					title='Pong Game'
+					width='800'
+					height='600'
+					style={{ border: 'none' }}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default App;
