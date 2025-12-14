cb1c56738cebe9af103b7087a6751b05a68b3664
Petra Bencze <petra.bncz@gmail.com>
2025-12-14 18:02:41 +0100

    feat: avatar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,16 @@ import { Suspense } from 'react';
 import { Link, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Home, About, NotFound, Loading, Game } from '@/src/pages/index';
+import { Avatar } from './components';
 
 function App() {
 	return (
 		<div className='min-h-screen bg-black p-6 text-pink-600'>
-			<nav className='font-retro-slim space-x-4'>
-				<Link to='/'>Home</Link>
+			<nav className='font-retro-slim flex justify-end'>
+				{/* <Link to='/'>Home</Link>
 				<Link to='/about'>About</Link>
-				<Link to='/game'>Pong</Link>
+				<Link to='/game'>Pong</Link> */}
+				<Link to='/profile'>{<Avatar />}</Link>
 			</nav>
 
 			<main className='mt-6'>
--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,7 +1,7 @@
 import type { FC } from 'react';
 
 const NotFound: FC = () => {
-	return <div className='text-center text-2xl font-bold'>404 - Page Not Found</div>;
+	return <div className='font-retro text-center text-2xl font-bold'>404 - Page Not Found</div>;
 };
 
 export default NotFound;
