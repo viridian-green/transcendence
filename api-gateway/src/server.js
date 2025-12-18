5c2e22437a1d59cbbc49ba63dd1e459518f082cc
Daiana <dairotchen@gmail.com>
2025-12-18 16:06:46 +0100

    fix(gateway server): make async, register all routes

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -1,6 +1,11 @@
 const fastify = require("fastify")({ logger: true });
 
-fastify.register(require("./plugins/auth"));
-fastify.register(require("./routes/proxy"));
+const start = async () => {
+  await fastify.register(require("./plugins/auth"));
+  await fastify.register(require("./routes/proxy"));
+  await fastify.register(require("./routes/game"));
 
-fastify.listen({ port: 3000, host: "0.0.0.0" });
+  await fastify.listen({ port: 3000, host: "0.0.0.0" });
+};
+
+start();
