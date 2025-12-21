41d26a28edef3cf05f431bc42584921e3eedee84
Natália Leite Schardosim <131466597+schardot@users.noreply.github.com>
2025-12-21 16:05:07 +0100

    fix(user-service): correct invalid PATCH route path

--- services/user-service/src/routes/users/update.js
+++ services/user-service/src/routes/users/update.js
@@ -1,5 +1,5 @@
 export default async function updateRoute(app) {
-    app.patch(':/id', async (req, reply) => {
+    app.patch('/:id', async (req, reply) => {
         const { id } = req.params;
         reply.send({ message: `Update user ${id}` });
     });
