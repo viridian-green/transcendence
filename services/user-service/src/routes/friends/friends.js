a99a954ced2aee50045d6faa487f2a8dda52b44d
Natália Leite Schardosim <131466597+schardot@users.noreply.github.com>
2025-12-24 14:17:14 +0100

    feat(user/friends): implemented POST /friends/:id route so authenticated users can send add requests

--- services/user-service/src/routes/friends/friends.js
+++ services/user-service/src/routes/friends/friends.js
@@ -1,3 +1,30 @@
 export default async function friendsRoute(app) {
+    //add friend
+    //POST /friends/:id
+    app.post('/:id', { preHandler: app.authenticate }, async (req, reply) => {
+        const userId = req.user.id;
+        const friendId = req.params.id;
 
+        // check if other user exists in db?
+        // check if friendship already exists
+        // check if userid == friendid
+        // check if friendship was already requested and its pending
+
+        const { rows } = await app.pg.query(`INSERT INTO friends (user_one, user_two)
+            VALUES ($1, $2)
+            RETURNING *`,
+        [userId, friendId]
+        );
+
+        const friendship = rows;
+        console.log(friendship);
+
+        return reply.code(200).send(rows[0]);
+    })
+
+    //remove friend
+    //DELETE /friends/:id
+
+    //get friends list
+    //GET /friends
 }
