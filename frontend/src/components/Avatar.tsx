1a8af7da966347351690fd152d01024dc18f3970
Petra Bencze <petra.bncz@gmail.com>
2025-12-29 18:41:55 +0100

    refactor: signout card UI

--- frontend/src/components/Avatar.tsx
+++ frontend/src/components/Avatar.tsx
@@ -3,16 +3,17 @@ import { useAuth } from '@hooks/useAuth.tsx';
 interface AvatarProps {
 	url?: string;
 	size?: number;
+	className?: string;
 }
 
-const Avatar = ({ url, size = 48 }: AvatarProps) => {
+const Avatar = ({ url, size = 48, className }: AvatarProps) => {
 	const { user } = useAuth();
 	const name = user?.username || '?';
 	const alt = `Avatar of ${name}`;
 	// const randomuserImage = 'https://randomuser.me/api/portraits/men/75.jpg';
 	return (
 		<div
-			className='overflow-hidden rounded-full border-2 border-pink-500 hover:opacity-50'
+			className={`overflow-hidden rounded-full border-2 border-pink-500 hover:opacity-50 ${className}`}
 			style={{ width: size, height: size }}
 		>
 			<img
--- frontend/src/pages/TopRightAvatar.tsx
+++ frontend/src/pages/TopRightAvatar.tsx
@@ -1,18 +1,27 @@
 import { Avatar, Exit, ProfileCircle } from '@/components';
 import { useAuth } from '@/hooks/useAuth';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const ProfileCard = () => {
 	const { user, signout } = useAuth();
+	const navigate = useNavigate();
 	return (
-		<div className='absolute top-14 right-0 flex flex-col items-start rounded-lg bg-slate-900 p-4 whitespace-nowrap text-slate-300 shadow-xs shadow-slate-700'>
-			<p>{user?.username}</p>
-			<div>
+		<div className='absolute top-14 right-0 flex flex-col rounded-lg bg-slate-900 p-4 whitespace-nowrap text-slate-300 shadow-xs shadow-slate-700'>
+			<p className='text-center text-pink-600'>{user?.username}</p>
+			<p className='mb-2 text-center text-sm text-pink-600'>{user?.email}</p>
+			<div
+				onClick={() => navigate('/profile')}
+				className='cursor-pointer rounded-lg border-0 px-2 hover:bg-[#0c8ce9]'
+			>
 				<p className='inline-block'>view profile</p>
 				<ProfileCircle className='ml-2 inline-block h-4 w-4 stroke-slate-300' />
 			</div>
 			<hr className='my-2 w-full border-slate-700' />
-			<div onClick={() => signout()} className='cursor-pointer'>
+			<div
+				onClick={() => signout()}
+				className='cursor-pointer rounded-lg border-0 px-2 hover:bg-[#0c8ce9]'
+			>
 				<p className='inline-block'>signout</p>
 				<Exit className='ml-2 inline-block h-4 w-4 stroke-slate-300' />
 			</div>
@@ -24,7 +33,7 @@ const TopRightAvatar = () => {
 	const [open, setOpen] = useState(false);
 	return (
 		<div onClick={() => setOpen(!open)} className='relative'>
-			<Avatar />
+			<Avatar className='cursor-pointer' />
 			{open && <ProfileCard />}
 		</div>
 	);
