128a7139df50bfc23e0f5f1a48f930dd354d869a
Petra Bencze <petra.bncz@gmail.com>
2026-01-01 12:07:45 +0100

    refactor: game start UI

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -3,3 +3,4 @@ export { default as PinkButton } from './PinkButton';
 export { default as ExclamationCircleOutline } from './ExclamationCircleOutline';
 export { default as Exit } from './Exit';
 export { default as ProfileCircle } from './ProfileCircle';
+export { default as ErrorMessage } from './ErrorMessage';
--- frontend/src/pages/GameStart.tsx
+++ frontend/src/pages/GameStart.tsx
@@ -1,10 +1,27 @@
-import { PinkButton } from '@/components';
+import { ErrorMessage, PinkButton } from '@/components';
+import { useAuth } from '@/hooks/useAuth';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+// TODO the logic here is that the logged in user always plays with w and s keys, maybe randomize logic
 const GameStart = () => {
 	const navigate = useNavigate();
+	const [opponent, setOpponent] = useState('');
+	const { user } = useAuth();
+	const [error, setError] = useState<string | null>(null);
 	const handleStartGame = () => {
+		if (opponent.length === 0) {
+			setError('Please provide an alias for your opponent');
+			return;
+		}
 		navigate('/game');
+	};
+
+	const handleValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setOpponent(e.target.value);
+		if (error) {
+			setError(null);
+		}
 	};
 
 	return (
@@ -15,36 +32,59 @@ const GameStart = () => {
 					Who are you playing with?
 				</label>
 				<input
+					className='border-border text-text-secondary w-64 rounded-md border-2 p-2'
 					id='opponent'
 					type='text'
 					placeholder="Enter opponent's alias"
-					className='border-border text-text-secondary w-64 rounded-md border-2 p-2'
+					value={opponent}
+					onChange={(e) => handleValueChange(e)}
 				/>
 				<div className='flex items-center justify-center gap-10'>
-					<div className='flex flex-col items-center gap-2'>
-						<p className='text-2xl'>You: </p>
-						<p className='text-2xl'>Left Paddle</p>
-						<div className='flex gap-1'>
-							<kbd className='bg-background rounded border px-2 py-1 text-sm shadow-sm'>
-								W
-							</kbd>
-							<kbd className='bg-background rounded border px-2 py-1 text-sm shadow-sm'>
-								S
-							</kbd>
+					<div className='bg-surface border-border shadow-elevated flex min-w-54 flex-col items-center gap-2 rounded-lg px-6 py-4 shadow'>
+						<p className='text-2xl'>{user?.username ?? 'You'}</p>
+						<div className='flex w-full items-center justify-between'>
+							<p className='text-text-secondary'>Paddle:</p>
+							<p className='text-xl'>Left</p>
+						</div>
+						<div className='flex w-full items-center justify-between'>
+							<p className='text-text-secondary'>Controls:</p>
+							<div className='flex gap-1'>
+								<kbd className='bg-background rounded border px-2 py-1 text-sm shadow-sm'>
+									W
+								</kbd>
+								<kbd className='bg-background rounded border px-2 py-1 text-sm shadow-sm'>
+									S
+								</kbd>
+							</div>
 						</div>
 					</div>
-					<div className='flex flex-col items-center gap-2'>
-						<p className='text-2xl'>Opponent: </p>
-						<p className='text-2xl'>Right Paddle</p>
-						<p className='text-2xl'>Up and Down keys</p>
+					<div className='bg-surface border-border shadow-elevated flex min-w-54 flex-col items-center gap-2 rounded-lg px-6 py-4 shadow'>
+						<p className='text-2xl'>{opponent.length > 0 ? opponent : 'Opponent'}</p>
+						<div className='flex w-full items-center justify-between'>
+							<p className='text-text-secondary'>Paddle:</p>
+							<p className='text-xl'>Right</p>
+						</div>
+						<div className='flex w-full items-center justify-between'>
+							<p className='text-text-secondary'>Controls:</p>
+							<div className='flex gap-1'>
+								<kbd className='bg-background rounded border px-2 py-1 text-sm shadow-sm'>
+									&uarr;
+								</kbd>
+								<kbd className='bg-background rounded border px-2 py-1 text-sm shadow-sm'>
+									&darr;
+								</kbd>
+							</div>
+						</div>
 					</div>
 				</div>
-				<p className='text-2xl'>First to X points wins!</p>
+				<p className='text-xl'>First to X points wins!</p>
+				{error && <ErrorMessage message={error} />}
 				<PinkButton
 					text='Start Game'
 					onClick={() => {
 						handleStartGame();
 					}}
+					className='text-accent-pink'
 				/>
 			</section>
 		</div>
