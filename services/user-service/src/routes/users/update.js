19a83641491e3c0424ec2b6ee549b7b7cc7cb66d
Daiana <dairotchen@gmail.com>
2026-01-06 16:07:33 +0100

    chore(login.js update.js): rm unused old files

--- services/user-service/src/routes/users/update.js
+++
@@ -1,6 +0,0 @@
-export default async function updateRoute(app) {
-    app.patch('/:id', async (req, reply) => {
-        const { id } = req.params;
-        reply.send({ message: `Update user ${id}` });
-    });
-}
