609378929da6b1139f965ab4dcf3c9c6f8ed4bfa
Daiana <dairotchen@gmail.com>
2026-01-14 15:49:13 +0100

    chore(user-serv): register route for users/state

--- services/user-service/src/routes/users-resources/index.js
+++ services/user-service/src/routes/users-resources/index.js
@@ -1,4 +1,6 @@
 import avatarRoutes  from "./avatar.routes.js";
+import userStateRoute from "./state.routes.js";
 export default async function userRoutes(app) {
-
+  await avatarRoutes(app);
+  await userStateRoute(app);
 }
