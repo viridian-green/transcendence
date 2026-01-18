85b655fac4838618831a49e13aab29e51751ceec
Natália Leite Schardosim <131466597+schardot@users.noreply.github.com>
2026-01-18 19:01:23 +0100

    feat: silence a bit of healthcheck noise

--- api-gateway/src/health.js
+++ api-gateway/src/health.js
@@ -1,5 +1,7 @@
 export default async function (app) {
-    app.get('/health', async () => {
+    app.get('/health', {
+        logLevel: 'silent'
+    }, async () => {
         return { status: "ok"};
     });
 }
