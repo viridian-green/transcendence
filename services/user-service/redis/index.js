7e869d14c8bfff152d86400f5a42073506fd0c34
ProjectDaiana <dairotchen@gmail.com>
2026-01-20 14:41:23 +0100

    fix(REDIS_PORTS): change to 6378 because 79 is busy in 42 machines

--- services/user-service/redis/index.js
+++ services/user-service/redis/index.js
@@ -2,7 +2,7 @@ import Redis from 'ioredis';
 
 const redis = new Redis({
 	host: process.env.REDIS_HOST || 'localhost',
-	port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
+	port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6378,
 	password: process.env.REDIS_PASSWORD || undefined,
 });
 
