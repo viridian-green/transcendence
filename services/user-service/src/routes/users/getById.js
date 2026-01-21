d7d8ec009283f73f401ab97f052b96f789a533a9
Natália Leite Schardosim <131466597+schardot@users.noreply.github.com>
2026-01-21 22:47:50 +0100

    feat: get usernames by ids endpoint

--- services/user-service/src/routes/users/getById.js
+++ services/user-service/src/routes/users/getById.js
@@ -1,6 +1,15 @@
+import { getUsernamesByIds } from "../../services/user.service.js";
+
 export default async function getByIdRoute(app) {
     app.get('/:id', async (req, reply) => {
         const { id } = req.params;
         reply.send({ message: `Get user ${id}` });
+    });
+
+    app.get('/', async (req, reply) => {
+        const { ids } = req.query;
+        let idsArray = ids.split(',');
+        const users = await getUsernamesByIds(app, idsArray);
+        reply.send({ users });
     });
 }
