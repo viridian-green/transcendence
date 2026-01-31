b9e1af570973ddd38c5b0d0293f23980ac008128
Petra Bencze <petra.bncz@gmail.com>
2026-01-31 21:36:39 +0100

    feat: Add seed script to populate initial data in user-service

--- services/user-service/src/server.js
+++ services/user-service/src/server.js
@@ -1,8 +1,10 @@
 import app from './app.js';
+import seed from './seed.js';
 
 const start = async() => {
     try {
         await app.listen({ port: 3003, host: "0.0.0.0" });
+		await seed();
         console.log("Server running on http://localhost:3003");
     } catch (err) {
         app.log.error(err);
