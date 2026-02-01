60c5100529fbbc0ca740cd7c3ab9b0d1401666d9
Daiana <dairotchen@gmail.com>
2026-02-01 18:33:35 +0100

    feat: remove seed function call from app initialization

--- services/user-service/src/app.js
+++ services/user-service/src/app.js
@@ -1,6 +1,5 @@
 import Fastify from 'fastify';
 import dotenv from 'dotenv';
-import seed from './seed.js';
 
 import plugins from './plugins/index.js';
 import routes from './routes/index.js';
@@ -16,7 +15,6 @@ app.register(plugins);
 app.register(routes);
 
 await app.ready();
-await seed();
 console.log(app.printRoutes());
 
 export default app;
