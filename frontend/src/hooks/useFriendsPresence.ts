a570eb8cd54dbe9c94e34d72b4e215da4cb9b7d3
Petra Bencze <petra.bncz@gmail.com>
2026-02-01 16:09:38 +0100

    feat: real-time update for Profile page and Remote page

--- frontend/src/hooks/useFriendsPresence.ts
+++ frontend/src/hooks/useFriendsPresence.ts
@@ -2,27 +2,36 @@
 import { useFriends } from './useFriends';
 import { useFetchOnlineUsers } from './useFetchOnlineUsers';
 import type { Friend } from '@/shared.types';
+import { usePresenceSocket } from './usePresenceSocket';
+import { useMemo } from 'react';
 
 export function useFriendsWithStatus(userId?: number) {
 	const { friends, error: friendsError, loading: friendsLoading } = useFriends(userId);
+	const { ws } = usePresenceSocket(Boolean(userId));
 	const {
 		users: onlineUsers,
 		error: onlineUsersError,
 		loading: onlineUsersLoading,
-	} = useFetchOnlineUsers(String(userId));
+	} = useFetchOnlineUsers(String(userId), ws.current);
 
 	console.log('[ONLINE USERS] friends:', JSON.stringify(friends, null, 2));
 
-	const friendsWithStatus: Friend[] = friends.map((friend) => {
-		const status: Friend['status'] = onlineUsers.some((u) => String(u.id) === String(friend.id))
-			? 'online'
-			: 'offline';
-		const friendWithStatus = {
-			...friend,
-			status: status,
-		};
-		return friendWithStatus;
-	});
+	const friendsWithStatus: Friend[] = useMemo(
+		() =>
+			friends.map((friend) => {
+				const status: Friend['status'] = onlineUsers.some(
+					(u) => String(u.id) === String(friend.id),
+				)
+					? 'online'
+					: 'offline';
+				const friendWithStatus = {
+					...friend,
+					status: status,
+				};
+				return friendWithStatus;
+			}),
+		[friends, onlineUsers],
+	);
 
 	console.log('[FETCHING FRIENDS] friends:', JSON.stringify(friends, null, 2));
 
