8f4b35c740f8c23c5177c7d5180b725b65c44e7e
ProjectDaiana <dairotchen@gmail.com>
2026-02-02 12:44:14 +0100

    hotfix(user redis port): update port;

--- services/user-service/src/redis/index.js
+++ services/user-service/src/redis/index.js
@@ -2,7 +2,7 @@ import Redis from "ioredis";
 
 const redis = new Redis({
     host: process.env.REDIS_HOST || "redis",
-    port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6378,
+    port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
     password: process.env.REDIS_PASSWORD || undefined,
 });
 
